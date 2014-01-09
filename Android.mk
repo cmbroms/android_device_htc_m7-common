6d07c1061ce42c5755e632e04d90ca50e9d40d23
Josh <lj50036@hotmail.com>
2014-01-08 20:54:09 -0600

    merge

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-ifneq ($(filter m7att m7spr m7tmo m7ul m7vzw,$(TARGET_DEVICE)),)
+ifneq ($(filter m7 m7att m7spr m7tmo m7ul m7vzw,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
@@ -33,7 +33,11 @@ FIRMWARE_MDM_IMAGES := \
     sbl1.mbn \
     sbl2.mbn
 
+<<<<<<< HEAD
 ifneq ($(filter m7att m7tmo m7ul,$(TARGET_DEVICE)),)
+=======
+ifneq ($(filter m7 m7att m7tmo m7ul,$(TARGET_DEVICE)),)
+>>>>>>> fe3fe21d58e96314e219d3e6ed4b83b956d01aff
 FIRMWARE_MDM_IMAGES += \
     htc61.mbn htc62.mbn htc63.mbn htc64.mbn htc65.mbn \
     htcnvbak.mbn htcrcust.mbn htcsmem.mbn \
